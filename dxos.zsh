652048a6e8df9b6ef3f58dbc5a66344d9f438bf2
Rich Burdon <rich.burdon@gmail.com>
2025-07-20 00:29:02 +0200

    Remove NX

--- dxos.zsh
+++ dxos.zsh
@@ -5,10 +5,4 @@
 alias gs='git-branch-select -l'
 alias gb='git branch -vv'
 
-#
-# pnpm nx run-many --target=build
-# pnpm -w nx build <target> --watch
-#
-
 alias pi="pnpm install"
-alias pw="pnpm watch"
